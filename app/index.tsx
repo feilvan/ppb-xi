e5c05ab412966a8899f8feab78d57860de67ebb8
Fuad Elhasan Irfani <feilvan@feilvan.com>
2026-09-05 08:30:15 +0700

    view, text, image

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,13 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ color: "#f00" }}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+      <Image
+        source={{ uri: "https://picsum.photos/200" }}
+        style={{ width: 200, height: 200 }}
+      />
     </View>
   );
 }
