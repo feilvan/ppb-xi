93f4fc98035d96ec23499a058adcd6075febee4b
Fuad Elhasan Irfani <feilvan@feilvan.com>
2026-09-19 11:01:00 +0700

    260919 flexbox, flatlist

--- app/flexbox.tsx
+++ app/flexbox.tsx
@@ -1,28 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function Flexbox() {
+  const siswa = [
+    { id: "1", nama: "Budi", kelas: "XI RPL" },
+    { id: "2", nama: "Andi", kelas: "XI RPL" },
+    { id: "3", nama: "Citra", kelas: "XI RPL" },
+  ];
+
   const style = StyleSheet.create({
     item: {
       padding: 8,
       backgroundColor: "#f00",
-      height: 50,
-      width: 50,
+      height: 70,
+      width: 70,
     },
   });
+
   return (
-    <View
-      style={{
-        display: "flex",
-        height: "100%",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 8,
-      }}
-    >
-      <Text style={style.item}>Item 1</Text>
-      <Text style={style.item}>Item 2</Text>
-      <Text style={style.item}>Item 3</Text>
+    <View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 8,
+        }}
+      >
+        <Text style={style.item}>Item 1</Text>
+        <Text style={style.item}>Item 2</Text>
+        <Text style={style.item}>Item 3</Text>
+      </View>
+      <View>
+        <FlatList
+          data={siswa}
+          renderItem={({ item }) => (
+            <Text style={style.item}>
+              {item.nama} {item.kelas}
+            </Text>
+          )}
+        />
+      </View>
     </View>
   );
 }
